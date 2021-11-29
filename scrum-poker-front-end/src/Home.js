345105c154c8c727222dc2d7ec1d3965155d8b9f
hadramet <hadramet.sylla@outlook.fr>
2021-11-29 16:59:05 +0100

    getting name from form

--- scrum-poker-front-end/src/Home.js
+++ scrum-poker-front-end/src/Home.js
@@ -1,9 +1,15 @@
-import { Typography, Button } from '@mui/material';
+import * as React from 'react';
+import { Typography, Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions } from '@mui/material';
 import { useCookies } from 'react-cookie';
 import { Box } from '@mui/system';
 import { randomBytes } from 'crypto';
 
+
 export function Home(props) {
+
+    const [open, setOpen] = React.useState(false);    
+    const [form, setForm] = React.useState({ name: ""});
+
     const [cookies, setCookie] = useCookies();
 
     const setUserId = () => {
@@ -11,12 +17,32 @@ export function Home(props) {
     };
 
     function handleSubmit(event) {
-        event.preventDefault();
+
+        event.preventDefault()
+
+        handleClose()
+
+        // TODO api call
         console.log({
-            user_id: cookies.user
+            type: 'create_room',
+            user_id: cookies.user,
+            user_display_name: form.name
         });
     }
 
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+
+    const handleChange = (e) => {
+        setForm((prevForm) => ({ ...prevForm, [e.target.name]: e.target.value }));
+    };
     return (<Box {...props}>
 
         <Typography component="h1" variant="h5">
@@ -27,12 +53,42 @@ export function Home(props) {
         <Button
             type="submit"
             fullWidth
-            onClick={handleSubmit}
-            variant="contained"
+            onClick={handleOpen}
+            variant="outlined"
             sx={{ mt: 3, mb: 2 }}
         >
             CREATE ROOM
         </Button>
+
+        <Dialog open={open} onClose={handleOpen} >
+            <DialogTitle> Create your room </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    All we need is a display name.
+                </DialogContentText>
+
+                <form component={form} onSubmit={handleSubmit}>
+
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        name="name"
+                        label="Display name"
+                        type="text"
+                        onChange={handleChange}
+                        fullWidth
+                        variant="standard" />
+
+                    <DialogActions>
+                        <Button onClick={handleClose}>CANCEL</Button>
+                        <Button type='submit' >CREATE ROOM</Button>
+                    </DialogActions>
+                </form>
+            </DialogContent>
+
+
+        </Dialog>
 
     </Box>
 
